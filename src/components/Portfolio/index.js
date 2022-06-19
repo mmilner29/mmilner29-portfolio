5d88d72b3504f6ec72ba0da2bad98715f8f7eb2c
mmilner29 <madisonm4514@gmail.com>
2022-06-18 21:03:53 -0700

    portfolio section, added projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,8 +2,35 @@ import React from "react";
 
 function Portfolio () {
     return (
-        <section>
-            <p>hello world</p>
+        <section className="project-section">
+            <div className="project">
+                <article className="proj">
+                    <h3 className="proj-centered">Web-Dev-Social</h3>
+                    
+                    <a href="https://github.com/Boris-Veselov/web-dev-social" target="_blank" id="img1" rel="noreferrer"><img src={require("../../assets/web-dev-social.png")} alt="Web-Dev-Social Home Page" ></img></a>
+                </article>
+            </div>
+            <div className="project">
+             <article className="proj">
+                    <h3 className="proj-centered">Run Buddy</h3>
+                
+                    <a href="https://github.com/mmilner29/run-buddy" target="_blank" rel="noreferrer"><img src={require("../../assets/run-buddy.png")} alt="Run-Buddy Website Preview"></img></a>
+                </article>
+            </div>
+            <div className="project">
+                <article className="proj">
+                    <h3 className="proj-centered">Recipe Hub</h3>
+                    
+                    <a href="https://github.com/mmilner29/the-recipe-hub" target="_blank" id="img1" rel="noreferrer"><img src={require("../../assets/recipe-hub.png")} alt="Recipe Hub Home Page" ></img></a>
+                </article>
+            </div>
+            <div className="project">
+                <article className="proj">
+                    <h3 className="proj-centered" id="white">Taskmaster Pro</h3>
+                    
+                    <a href="https://github.com/mmilner29/taskmaster-pro" target="_blank" id="img1" rel="noreferrer"><img src={require("../../assets/taskmaster-pro.png")} alt="Taskmaster Pro Home Page" ></img></a>
+                </article>
+            </div>
         </section>
 
     )
