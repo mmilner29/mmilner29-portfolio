390a56110420ab46e76c8d2cf4139fb5a5ad46f3
mmilner29 <madisonm4514@gmail.com>
2022-07-31 18:14:18 -0700

    add modal to project section

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,13 +1,37 @@
 import React from "react";
+import Modal from "../Modal";
 
-function Portfolio () {
+class Portfolio extends React.Component {
+    state = {
+        show: false
+      };
+      showModal = e => {
+        this.setState({
+          show: !this.state.show
+        });
+      };
+      
+    render() {
+        
     return (
         <section className="project-section">
             <div className="project">
                 <article className="proj">
-                    <h3 className="proj-centered" id="white">Lucky-Duck</h3>
                     
-                    <a href="https://github.com/CarlBush/Lucky-Duck" target="_blank" id="img1" rel="noreferrer"><img src={require("../../assets/lucky-duck.png")} alt="Lucky-Duck Home Page" ></img></a>
+                    <div id="img1" class="toggle-button"
+                        onClick={e => {
+                        this.showModal();
+                    }}><img src={require("../../assets/lucky-duck.png")} alt="Lucky-Duck Home Page" ></img></div>
+                    <button class="toggle-button"
+                        id="centered-toggle-button"
+                        onClick={e => {
+                        this.showModal();
+                    }}
+                    > <h3 className="proj-centered" id="white">Lucky-Duck</h3></button>
+                    <Modal onClose={this.showModal} show={this.state.show}>
+                    <a href="https://github.com/mmilner29/Lucky-Duck" target="_blank" rel="noreferrer">View Github</a>
+                    <a href="https://scenic-great-basin-78312.herokuapp.com/" target="_blank" rel="noreferrer">View Deployed Project</a>
+                    </Modal>
                 </article>
             </div>
             <div className="project">
@@ -41,6 +65,7 @@ function Portfolio () {
         </section>
 
     )
+}
 }
 
 export default Portfolio;
